dcb49ba4c0fec933dc7238ade4780dc7726cb2fc
Joanna Zielinska <joannazielinskam@gmail.com>
2022-09-12 12:22:14 +0200

    Added user sginup logic

--- record-market/routes/auth.js
+++ record-market/routes/auth.js
@@ -8,8 +8,34 @@ router.get('/signup', (req, res, next) => {
      res.render('signup')
 });
 
+router.post("/signup", (req,res,next) => {
+	const { username, password } = req.body
+	
+	if (password.length < 6) {
+		res.render('signup', { message: 'Your password needs to be min 6 chars' })
+		return
+	}
+	if (username.length === 0) {
+		res.render('signup', { message: 'Your username cannot be empty' })
+		return
+	}
 
-
-
+	User.findOne({ username: username })
+		.then(userFromDB => {
+			if (userFromDB !== null) {
+				res.render('signup', { message: 'Username is alredy taken' })
+			} else {
+				const salt = bcrypt.genSaltSync()
+				const hash = bcrypt.hashSync(password, salt)
+				// create the user
+				User.create({ username, password: hash })
+					.then(createdUser => {
+						console.log(createdUser)
+						res.redirect('/login')
+					})
+					.catch(err => next(err))
+			}
+		})
+});
   
 module.exports = router;
