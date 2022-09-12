2dd584ce24306d659725694e43b76ecf04bee866
Joanna Zielinska <joannazielinskam@gmail.com>
2022-09-12 21:45:44 +0200

    added basic collection edit

--- record-market/routes/collections.js
+++ record-market/routes/collections.js
@@ -38,7 +38,7 @@ router.post('/:id/add', loginCheck(), (req, res, next) => {
 
 //collections/remove/{{_id}}
 
-router.get('/remove/:idCollection', loginCheck(), (req,res,next) => {
+router.get('/remove/:idCollection', loginCheck(), (req, res, next) => {
     Collection.findByIdAndDelete(req.params.idCollection)
             .then(()=> {
                 res.redirect('/profile')
@@ -46,7 +46,7 @@ router.get('/remove/:idCollection', loginCheck(), (req,res,next) => {
             .catch(err => next(err))
 })
 
-router.get('/collection/:idCollection', loginCheck(), (req,res,next) => {
+router.get('/collection/:idCollection', loginCheck(), (req, res, next) => {
     Collection.findById(req.params.idCollection)
             .then(collectionByID => {
                 res.render('collections/collection', { collection: collectionByID })
@@ -54,12 +54,23 @@ router.get('/collection/:idCollection', loginCheck(), (req,res,next) => {
             .catch(err => next(err))
 })
 
-router.get('/edit/:idCollection', loginCheck(), (req,res,next) => {
-    Collection.findByIdAndDelete(req.params.idCollection)
-            .then(()=> {
-                res.redirect('/profile')
+router.get('/edit/:idCollection', loginCheck(), (req, res, next) => {
+    Collection.findById(req.params.idCollection)
+            .then((collectionByID) => {
+                res.render('collections/edit', { collection: collectionByID })
             })
             .catch(err => next(err))
+})
+
+router.post('/edit/:idCollection', loginCheck(), (req, res, next) => {
+     const { name, description } = req.body
+     Collection.findByIdAndUpdate(req.params.idCollection, {
+        name,
+        description
+    })
+    .then (() => {
+        res.redirect(`/collections/collection/${req.params.idCollection}`)
+    })
 })
 
 
