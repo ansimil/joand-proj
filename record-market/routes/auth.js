113f59170b74e77612e811bb9d5aff9a252617db
Joanna Zielinska <joannazielinskam@gmail.com>
2022-09-12 15:07:08 +0200

    added loginCheck middleware

--- record-market/routes/auth.js
+++ record-market/routes/auth.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const User = require('../models/User.model')
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
+const loginCheck = require('../utils/loginCheck')
+
 
 
 router.get('/signup', (req, res, next) => {
@@ -59,6 +61,11 @@ router.get('/logout', (req, res, next) => {
     req.session.destroy()
     res.redirect('/')    
        
+});
+
+
+router.get('/test', loginCheck(), (req, res, next) => {
+    res.render('test')
 });
 
 
