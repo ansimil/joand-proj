e844fc323c984783827d856559a3d020bf517aea
John Doe <AndrewSimonMiller@Andrews-MacBook-Pro.local>
2022-09-12 18:07:12 +0200

    useless search features

--- record-market/routes/albums.js
+++ record-market/routes/albums.js
@@ -2,31 +2,53 @@ const router = require("express").Router();
 const { default: axios } = require("axios");
 const consumerKey = process.env.CONSUMER_KEY
 const secretKey = process.env.SECRET_KEY
+var Discogs = require('disconnect').Client;
+
+var dis = new Discogs({
+	consumerKey: consumerKey, 
+	consumerSecret: secretKey
+});
+
+var db = dis.database();
+
 
 router.get('/album/:name/:id', (req,res,next) => {
     let albumArr = []
-    axios.get(`https://api.discogs.com/masters/${req.params.id}`)
-    .then (response => {
-      //console.log(response.data)
-      
-    axios.get(`https://api.discogs.com/database/search?artist=${req.params.name}&format=album&key=xxngcrMgfixhjsPCBABk&secret=BHHlWipWQaZBkHAuJhBVAkxLOYZWgYlR`)
-    .then (response2 => {
-    //console.log(req.params.name)
-    if (response.data.artists[0].name === req.params.name){ 
-        //console.log(response2.data)
-    response2.data.results.forEach(album => {
-        if(album.title === `${req.params.name} - ${response.data.title}`) {
-            console.log(album)
-            albumArr.push(album.cover_image)
-        }
-    })
+    db.getRelease(req.params.id, function(err, data){
+        // console.log(data);
+        if(data.message !== 'Release not found.'){
+        let img = data.images[0].resource_url
+        res.render('albumTracks', {tracks: data, imgSrc: img})
     }
-    res.render('albumTracks', {tracks: response.data, albums: albumArr[0]})
-    })
+    else {
+        res.render('albumTracks') 
+    }
+        //console.log('done')
+    });
 
-      })
-    .catch(err => console.log(err))
-  }) 
+})
+    // axios.get(`https://api.discogs.com/masters/${req.params.id}`)
+    // .then (response => {
+    //   //console.log(response.data)
+      
+    // axios.get(`https://api.discogs.com/database/search?artist=${req.params.name}&format=album&key=xxngcrMgfixhjsPCBABk&secret=BHHlWipWQaZBkHAuJhBVAkxLOYZWgYlR`)
+    // .then (response2 => {
+    // //console.log(req.params.name)
+    // if (response.data.artists[0].name === req.params.name){ 
+    //     //console.log(response2.data)
+    // response2.data.results.forEach(album => {
+    //     if(album.title === `${req.params.name} - ${response.data.title}`) {
+    //         console.log(album)
+    //         albumArr.push(album.cover_image)
+    //     }
+    // })
+    // }
+    // res.render('albumTracks', {tracks: response.data, albums: albumArr[0]})
+    // )
+
+    //   })
+    // .catch(err => console.log(err))
+  
 
 
 
--- record-market/routes/artists.js
+++ record-market/routes/artists.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 const { default: axios } = require("axios");
 const consumerKey = process.env.CONSUMER_KEY
 const secretKey = process.env.SECRET_KEY
+var Discogs = require('disconnect').Client;
+
+var dis = new Discogs({
+	consumerKey: consumerKey, 
+	consumerSecret: secretKey
+});
+
+var db = dis.database();
 
 router.get("/artists", (req, res, next) => {
   let arr = []
@@ -24,24 +32,39 @@ router.get("/artists", (req, res, next) => {
 
 router.get('/artist/:name/:id', (req,res,next) => {
   let albumArr = []
-  axios.get(`https://api.discogs.com/database/search?artist=${req.params.name}&format=album&key=${consumerKey}&secret=${secretKey}`)
-  .then (response => {
-    //console.log(response.data)
-    response.data.results.forEach(album => {
-      if (album.type === 'master'){
-        albumArr.push(album)
-      }
-    })
-    //console.log(albumArr)
-  axios.get(`https://api.discogs.com/artists/${req.params.id}`)
-  .then(response => {
-  //console.log(albumArr)
-  res.render('artistAlbums', {albums: albumArr, artist: response.data})
-  })
-  .catch(err => console.log(err)) 
-  })
-  .catch(err => console.log(err))
-}) 
+  db.getArtistReleases(req.params.id, {page: 1, per_page: 1000}, function(err, data){
+        data.releases.forEach(album => {
+          if (album.type === 'master' && album.thumb !== ''){
+            albumArr.push(album)
+          }
+        })
+        db.getArtist(req.params.id, function(err, data2){
+          //console.log(data2)  
+          res.render('artistAlbums', {albums: albumArr, artist: data2})
+        })
+        
+      })
+    
+  }) 
+  // let albumArr = []
+  // axios.get(`https://api.discogs.com/artists/${req.params.id}/releases`)
+  // .then (response => {
+  //   //console.log(response.data)
+  //   response.data.releases.forEach(album => {
+  //     if (album.type === 'release'){
+  //       albumArr.push(album)
+  //     }
+  //   })
+  //   //console.log(albumArr)
+  // axios.get(`https://api.discogs.com/artists/${req.params.id}`)
+  // .then(response => {
+  // //console.log(albumArr)
+  // res.render('artistAlbums', {albums: albumArr, artist: response.data})
+  // })
+  // .catch(err => console.log(err)) 
+  // })
+  // .catch(err => console.log(err))
+ 
 
 
 
