6aca2198b006038f770c176ea20b7bfef7e0b8e2
Joanna Zielinska <joannazielinskam@gmail.com>
2022-09-14 11:26:23 +0200

    changes

--- record-market/routes/albums.js
+++ record-market/routes/albums.js
@@ -21,33 +21,12 @@ var db = dis.database();
 
 router.get('/album/:id', (req,res,next) => {
     let albumArr = []
-    let userCoord = req.user.coordinates
     db.getMaster(req.params.id, function(err, data){
         if(data.message !== 'Release not found.'){
-        //console.log(data)
-    let collectionArr = []
-    Album.find({ 'discogsId': req.params.id })
-    .then(data => {
-        data.forEach(album => {
-        Collection.find({'albums' : album._id})
-        .then(data2 => {
-            data2.forEach(collection => {
-            User.find({'collections': collection._id})
-            .then(data3 => {
-                    console.log(data3)
-                })    
-            })
-        })
-        })
-    })       
-    
-    
-    db.getMaster(req.params.id, function(err, data){
-        if(data.message !== 'Release not found.'){
-
+        console.log(data)
         if (!data.videos && data.images){
         let img = data.images[0].resource_url
-        res.render('./artistsAlbumsTracks/albumTracks', {tracks: data, imgSrc: img, auth: req.isAuthenticated(), user: userCoord})}
+        res.render('./artistsAlbumsTracks/albumTracks', {tracks: data, imgSrc: img, auth: req.isAuthenticated()})}
         else if (data.videos && data.images){
             let img = data.images[0].resource_url
             let vids = []
@@ -56,31 +35,34 @@ router.get('/album/:id', (req,res,next) => {
             })
             //console.log(vids)
             //let vid = data.videos[0].uri.replace("watch?v=", "embed/") 
-            res.render('./artistsAlbumsTracks/albumTracks', {tracks: data, imgSrc: img, vids: vids, auth: req.isAuthenticated(), user: userCoord}) 
+            res.render('./artistsAlbumsTracks/albumTracks', {tracks: data, imgSrc: img, vids: vids, auth: req.isAuthenticated()}) 
 
         }
     }
     else {
         res.render('albumTracks') 
     }
+        //console.log('done')
     });
 
 })
 
 router.get('/album/:id/add', loginCheck(), (req, res, next) =>{
     const userId = req.user._id
+    //console.log(userId)
     User.findById(userId)
     .populate('collections')
     .then(user => {
+        //console.log(user)
         res.render('./artistsAlbumsTracks/addAlbumToCollection', {user: user, auth: req.isAuthenticated(), releaseId: req.params.id})
     })
     .catch(err => next(err))
     })
 
 router.post('/album/:id/add', loginCheck(), (req, res, next)=>{
+
     db.getMaster(req.params.id, function(err, data){
-        //console.log(req.params.id) 
-        const discogsId = req.params.id    
+        console.log(data.artists[0].name)     
         const name = data.title
         const artist = data.artists[0].name
         const imgName = data.images[0].type
@@ -93,7 +75,7 @@ router.post('/album/:id/add', loginCheck(), (req, res, next)=>{
             tracks.push({name: track.title, duration: track.duration})
         })   
     const collectionID = req.body.collection
-    Album.create({ name, artist, imgName, imgPath, release, price, userPrice, tracks, discogsId })
+    Album.create({ name, artist, imgName, imgPath, release, price, userPrice, tracks })
             .then((createdAlbum) => {
                 Collection.findByIdAndUpdate(collectionID, {
                     $push: { albums: [createdAlbum] }
